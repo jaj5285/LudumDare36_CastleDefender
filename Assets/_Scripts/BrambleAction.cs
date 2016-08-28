50ce2eb059cd6097b8de8037479fb97b6bf5c24c
Andrew Shirtz <anshirtz@gmail.com>
2016-08-28 15:09:34 -0700

    Changed Bramble Gen to handle slopes. Brambles now slow enemies. Added isKillable bool to Construction so Enemies won't attack statues or brambles.

---
+++ Assets/_Scripts/BrambleAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrambleAction : MonoBehaviour {
+
+	public float brambleMoveSpeed = 2.5f;
+	public float standardMoveSpeed = 5f;
+
+	void OnTriggerStay (Collider other) {
+		if (other.tag == "Enemy") {
+			other.GetComponent<EnemyBehavior> ().travelSpeed = brambleMoveSpeed;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Enemy") {
+			other.GetComponent<EnemyBehavior> ().travelSpeed = standardMoveSpeed;
+		}
+	}
+}
